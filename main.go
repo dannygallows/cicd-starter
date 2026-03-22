1efd2d591b6da3b873e4f6f415786ec781ce49ff
dannygallows <43937866+dannygallows@users.noreply.github.com>
2026-03-22 08:45:49 +0100

    fix a formatting issue

--- main.go
+++ main.go
@@ -91,11 +91,11 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
 		ReadHeaderTimeout: 5 * time.Second,
 	}
-	
+
 	cleanPort := strings.ReplaceAll(strings.ReplaceAll(port, "\n", ""), "\r", "")
 	log.Printf("Serving on port: %s\n", cleanPort)
 	log.Fatal(srv.ListenAndServe())
